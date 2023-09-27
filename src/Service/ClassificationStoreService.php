f88e23b8da75b4acac69e7e20ffd42948aef71f6
Purshottam Nepal <prunepal333@gmail.com>
2023-09-27 12:21:02 +0545

    Add strict type declaration

--- src/Service/ClassificationStoreService.php
+++ src/Service/ClassificationStoreService.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types = 1);
+
 namespace Purush\CstoreBundle\Service;
 use Pimcore\Model\DataObject\Classificationstore\CollectionConfig;
 use Pimcore\Model\DataObject\Classificationstore\CollectionGroupRelation;
